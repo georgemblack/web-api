1322613ac03ee346cda6ae160d45a92e7584e7e1
georgemblack <contact@georgeblack.me>
2019-11-23 17:36:15 -0600

    Create example write to Firestore

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+const admin = require('firebase-admin')
 const express = require('express')
+
+// Firestore connection
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
+const db = admin.firestore();
+
+// Express
 const app = express()
 const port = process.env.PORT
 
 app.get('/', (req, res) => res.send('Hello, Cloud Run!'))
 
-app.get('/example', (req, res) => res.send('Example endpoint'))
+app.get('/example', (req, res) => {
+  let exampleDocRef = db.collection('personal-web-views').doc('test-doc-id');
+  let result = exampleDocRef.set({
+    'someKey': 'someValue',
+    'someOtherKey': 'someOtherValue'
+  })
+  res.send('Thanks for visiting :)')
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
