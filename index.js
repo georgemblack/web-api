96b1b81a53dd4d35c9161d66168376b6522dfe7e
georgemblack <contact@georgeblack.me>
2019-11-23 17:40:33 -0600

    Add minimal linter and fix lint errors

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require('express')
 // Firestore connection
 admin.initializeApp({
   credential: admin.credential.applicationDefault()
-});
-const db = admin.firestore();
+})
+const db = admin.firestore()
 
 // Express
 const app = express()
@@ -14,10 +14,10 @@ const port = process.env.PORT
 app.get('/', (req, res) => res.send('Hello, Cloud Run!'))
 
 app.get('/example', (req, res) => {
-  let exampleDocRef = db.collection('personal-web-views').doc('test-doc-id');
-  let result = exampleDocRef.set({
-    'someKey': 'someValue',
-    'someOtherKey': 'someOtherValue'
+  const exampleDocRef = db.collection('personal-web-views').doc('test-doc-id')
+  exampleDocRef.set({
+    someKey: 'someValue',
+    someOtherKey: 'someOtherValue'
   })
   res.send('Thanks for visiting :)')
 })
