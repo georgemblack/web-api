018f3a9562b64d63a029b196e1d19ce23e902931
georgemblack <contact@georgeblack.me>
2020-01-16 19:58:56 -0600

    Add support for client hints

--- src/app.js
+++ src/app.js
@@ -10,20 +10,22 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://georgeblack.me')
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Accept-CH', 'UA, Platform, Model, Arch, Viewport-Width, Width')
+  res.header('Accept-CH-Lifetime', '2592000')
   next()
 })
 
 app.options((req, res) => {
-  res.send(200)
+  res.sendStatus(200)
+})
+
+app.get('/', (req, res) => {
+  res.sendStatus(200)
 })
 
 app.post('/views', async (req, res) => {
   // validate payload
   if (
-    typeof req.body.userAgent !== 'string' ||
-    req.body.userAgent === '' ||
-    typeof req.body.hostname !== 'string' ||
-    req.body.hostname === '' ||
     typeof req.body.pathname !== 'string' ||
     req.body.pathname === '' ||
     typeof req.body.referrer !== 'string' ||
@@ -35,17 +37,28 @@ app.post('/views', async (req, res) => {
     return res.status(400).send('Validation failed')
   }
 
+  // build client hints if available
+  const clientHints = {}
+  if (req.get('Sec-CH-UA')) clientHints.userAgent = req.get('Sec-CH-UA')
+  if (req.get('Sec-CH-Platform')) clientHints.platform = req.get('Sec-CH-Platform')
+  if (req.get('Sec-CH-Model')) clientHints.model = req.get('Sec-CH-Model')
+  if (req.get('Sec-CH-Arch')) clientHints.arch = req.get('Sec-CH-Arch')
+  if (req.get('Sec-CH-Viewport-Width')) clientHints.viewportWidth = req.get('Sec-CH-Viewport-Width')
+  if (req.get('Sec-CH-Width')) clientHints.width = req.get('Sec-CH-Width')
+
+  // primary payload
   const docPayload = {
-    userAgent: req.body.userAgent,
-    hostname: req.body.hostname,
+    hostname: req.hostname,
     pathname: req.body.pathname,
     windowInnerWidth: req.body.windowInnerWidth,
     timezone: req.body.timezone,
     timestamp: new Date()
   }
 
-  // append referrer if non-empty
-  if (req.body.referrer !== '') docPayload.referrer = req.body.referrer
+  // append possibly empty items
+  if (req.get('User-Agent')) docPayload.userAgent = req.get('User-Agent')
+  if (req.body.referrer) docPayload.referrer = req.body.referrer
+  if (Object.keys(clientHints).length !== 0) docPayload.clientHints = clientHints
 
   // write to firestore
   try {
