07d32442e129b33b82c61f9916684c561e7c8273
georgemblack <contact@george.black>
2021-01-31 22:39:04 -0600

    Dependency bump

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,6 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true,
-  },
   extends: ["plugin:prettier/recommended"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
   parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {},
+    ecmaVersion: 2021,
+  }
 };
