928f87f84134bbe9274961179c05d502cb667e49
George Black <contact@george.black>
2024-03-23 15:21:01 +0000

    Add initial API test

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -18,15 +18,24 @@ func Run() error {
 		return types.WrapErr(err, "failed to create firestore service")
 	}
 
-	// Configure gin & default middlewares
+	r := setupRouter(config)
+
+	return r.Run()
+}
+
+func setupRouter(config conf.Config) *gin.Engine {
 	r := gin.Default()
 	r.Use(getHeaderMiddleware(config))
 
+	// Basic auth
+	r.POST("/auth", getAuthHandler(config))
+
+	// Standard endpoints
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello",
 		})
 	})
 
-	return r.Run()
+	return r
 }
